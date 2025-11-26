144de52ec9762db799da9f26ff5b680789ba5f64
unknown <136925827+francoGabrielweb@users.noreply.github.com>
2025-11-26 14:15:54 -0300

    todo la coneccion fue correcta con la api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from  "cors"
 import  dotenv from "dotenv"
+import { pool } from './src/config/db.js';
 
 
 dotenv.config()
@@ -13,6 +14,29 @@ const PATH = process.env.PORT
 const  app =  express()
 
 app.use(cors())
+
+app.get("/api/users" , async (req , res)=> {
+
+    const query = "SELECT * FROM users"
+
+
+    const [rows] =  await pool.query(query)
+
+     res.status(200).json({
+      success: true,
+      data: rows
+    });
+
+
+
+
+
+
+
+   
+
+
+})
 
 app.listen(PATH , ()=> {
   
--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,6 @@
-import mysql from "mysql2"
-
+import mysql from "mysql2/promise.js"
+import dotenv from "dotenv"
+dotenv.config() ;
 
 export const pool  =  mysql.createPool({
    
